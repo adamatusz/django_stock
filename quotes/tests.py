11a3e7bda7d8bea2749ab75cc96b225b0f3be0d5
adamatusz@gmail.com <adamatusz@gmail.com>
2022-07-09 09:04:52 +0200

    pytest logout_login

--- quotes/tests.py
+++ quotes/tests.py
@@ -36,6 +36,40 @@ def test_home_view(client):  # klient przeglądarka podawany domyślnie przez py
     assert response.status_code == 200
 
 
+def test_about_view(client):  # klient przeglądarka podawany domyślnie przez pytest
+    response = client.get(reverse('about'))
+    print(response.content)
+    assert response.status_code == 200
+
+
+def test_login_view(client):  # klient przeglądarka podawany domyślnie przez pytest
+    response = client.get(reverse('login'))
+    print(response.content)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_login_view(client):  # klient przeglądarka podawany domyślnie przez pytest
+    client.login(username='adam',
+                 password='coderslab')
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
+
+
+def test_signup_view(client):  # klient przeglądarka podawany domyślnie przez pytest
+    response = client.get(reverse('signup'))
+    print(response.content)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_signup_view(client):  # klient przeglądarka podawany domyślnie przez pytest
+    client.login(username='adam',
+                 password='coderslab')
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
+
+
 def test_add_stock_view_with_not_logged_user(client):
     response = client.get(reverse('add_stock'))
     assert response.status_code == 302  # oczekujemy przekierowania na @loginrequire
@@ -72,4 +106,24 @@ def test_add_stock_with_logged_user(client, user):
     post_response = client.post(reverse('add_stock'), payload, follow=True)
     print(dir(post_response))
     assert post_response.status_code == 200
+
+
+def test_delete_stock_view_with_not_logged_user(client):
+    response = client.get(reverse('delete_stock'))
+    assert response.status_code == 302  # oczekujemy przekierowania na @loginrequire
+
+
+@pytest.mark.django_db
+def test_delete_stock_view_with_logged_user(client, user):
+    client.login(username='adam',
+                 password='coderslab')
+    response = client.get(reverse('delete_stock'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_stock(stock):
+    stocks = Stock.objects.all()
+    assert len(stocks) == 1
+    assert stocks.first().ticker == 'amzn'  # bardziej testujemy Django
 
